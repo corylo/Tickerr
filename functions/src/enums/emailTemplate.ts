dc6c901c0a07ce8e7e95f8d476ee033481570643
Cory Loeffelman <coryloeffelman@gmail.com>
2021-04-04 18:36:17 -0500

    Updated email template urls and project id

--- functions/src/enums/emailTemplate.ts
+++ functions/src/enums/emailTemplate.ts
@@ -1,4 +1,4 @@
 export enum EmailTemplate {
-  Welcome = "https://firebasestorage.googleapis.com/v0/b/tickerr-tv.appspot.com/o/emails%2Ftemplates%2Fwelcome.html?alt=media&token=42132f2b-b1fd-4497-a62a-a69bef00cc07",
+  Welcome = "https://firebasestorage.googleapis.com/v0/b/tickerr-tv.appspot.com/o/emails%2Ftemplates%2Fwelcome.html?alt=media&token=e206773a-ea7a-4054-9459-28071e531f18",
   Goodbye = "https://firebasestorage.googleapis.com/v0/b/tickerr-tv.appspot.com/o/emails%2Ftemplates%2Fgoodbye.html?alt=media&token=d33429a8-5e30-4a35-bc68-3d0599e67a60"
 }
