d578b3a0a1c774c71a8d4eed5b42aef135084291
Cory Loeffelman <coryloeffelman@gmail.com>
2021-04-04 17:39:53 -0500

    Sign in disclaimer

--- tickerr-client/components/signInModal/signInModal.tsx
+++ tickerr-client/components/signInModal/signInModal.tsx
@@ -50,6 +50,9 @@ export const SignInModal: React.FC<SignInModalProps> = (props: SignInModalProps)
             <img src={`${CDN.Url}/img/brands/google-logo.png`} />
             <h1 className="passion-one-font">Sign In</h1>
           </Button>
+          <div id="tickerr-sign-in-disclaimer">
+            <h1 className="passion-one-font">By signing in you agree to our <a href="https://legal.tickerr.tv/privacy">Privacy Policy</a> and <a href="https://legal.tickerr.tv/terms">Terms & Conditions</a></h1>
+          </div>
         </ModalBody>
       </Modal>
     );
