e87d84fbc00ecee78466c2bdb8d6cb88fc7ad5c8
Cory Loeffelman <coryloeffelman@gmail.com>
2021-04-18 20:35:37 -0500

    Sort search results

--- tickerr-client/components/searchBar/searchBar.tsx
+++ tickerr-client/components/searchBar/searchBar.tsx
@@ -1,5 +1,6 @@
 import classNames from "classnames";
 import React, { useContext, useReducer, useRef } from "react";
+import _sortBy from "lodash.sortby";
 
 import { IconButton } from "../buttons/iconButton";
 import { SearchBarInput } from "./components/searchBarInput/searchBarInput";
@@ -61,7 +62,7 @@ export const SearchBar: React.FC<SearchBarProps> = (props: SearchBarProps) => {
 
   const getResults = (): JSX.Element => {
     if(focused) {
-      const links: JSX.Element[] = results.map((coin: IGeckoCoinSymbolMapItem, i: number) => (
+      const links: JSX.Element[] = _sortBy(results, "name").map((coin: IGeckoCoinSymbolMapItem, i: number) => (
         <SearchLink
           key={coin.symbol} 
           coin={coin}
