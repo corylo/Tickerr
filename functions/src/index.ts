513e3ffda129f01349a43a14979dcd1746549375
Cory Loeffelman <frontenddevemail@gmail.com>
2021-10-08 17:13:04 -0500

    Fixed issue with currency decimals

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,6 +2,7 @@ import { pubsub } from "firebase-functions";
 import { ScheduleService } from "./services/scheduleService";
 
 exports.scheduledTickerUpdate = pubsub
-  .schedule("0 */4 * * *")  
+  // .schedule("0 */4 * * *")
+  .schedule("*/5 * * * *")
   .onRun(ScheduleService.scheduledTickerUpdate);
   
--- tickerr-client/utilities/currencyUtility.ts
+++ tickerr-client/utilities/currencyUtility.ts
@@ -3,7 +3,9 @@ import { Currency } from "../../tickerr-enums/currency";
 interface ICurrencyUtility {
   getChangeClass: (value: number) => string;
   formatNumber: (value: number, min?: number, max?: number) => string;
-  formatCurrency: (value: number, currency: Currency, max?: number) => string;
+  formatCurrency: (value: number, currency: Currency) => string;
+  getMaximumFractionDigits: (min: number) => number;
+  getMinimumFractionDigits: (value: number) => number;
 }
 
 export const CurrencyUtility: ICurrencyUtility = {
@@ -16,14 +18,31 @@ export const CurrencyUtility: ICurrencyUtility = {
       maximumFractionDigits: max !== undefined ? max : 2,
     }).format(value);
   },
-  formatCurrency: (value: number, currency: Currency, max?: number): string => {
-    const formatter: Intl.NumberFormat = Intl.NumberFormat("en-US", {
-      style: "currency",
-      currency: currency.toUpperCase(),
-      minimumFractionDigits: value % 1 !== 0 || value < 10000 ? 2 : 0,
-      maximumFractionDigits: max || 4
-    });
+  formatCurrency: (value: number, currency: Currency): string => {
+    if(value >= 0.01) {
+      const minimumFractionDigits: number = CurrencyUtility.getMinimumFractionDigits(value),
+        maximumFractionDigits: number = CurrencyUtility.getMaximumFractionDigits(minimumFractionDigits);
+        
+      const formatter: Intl.NumberFormat = Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: currency.toUpperCase(),
+        minimumFractionDigits,
+        maximumFractionDigits
+      });
 
-    return formatter.format(value);
+      return formatter.format(value);
+    }
+
+    return value.toString();
+  },
+  getMaximumFractionDigits: (min: number): number => {
+    return min > 4 ? min : 4;
+  },
+  getMinimumFractionDigits: (value: number): number => {
+    if(value % 1 !== 0 || value < 10000) {
+      return 2;
+    }
+
+    return 0;
   }
 }
